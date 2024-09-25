a17f5cc56c03840d93c66dc69874852d7b034aa5
thedoynewton <thedoynewton@gmail.com>
2024-09-26 00:46:42 +0800

    change to Total Messages Sent

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,7 +11,7 @@
         <!-- Number of Messages and Balance -->
         <div class="mb-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <div class="bg-blue-100 p-4 rounded-lg">
-                <h2 class="text-xl font-bold">Total Messages Sent to Recipients</h2>
+                <h2 class="text-xl font-bold">Total Messages Sent</h2>
                 <p class="text-2xl font-semibold">{{ $totalSent }}</p>
             </div>
             <div class="bg-indigo-100 p-4 rounded-lg">
